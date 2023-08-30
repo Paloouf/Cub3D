4f71318e7f4c91532eed7a52de162ae0efe6624d
ltressen <paloouf@gmail.com>
2023-08-30 15:55:11 +0200

    yolo

--- cub3d.h
+++ cub3d.h
@@ -6,13 +6,27 @@
 /*   By: ltressen <ltressen@student.42perpignan.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/30 13:11:49 by ltressen          #+#    #+#             */
-/*   Updated: 2023/08/30 13:12:35 by ltressen         ###   ########.fr       */
+/*   Updated: 2023/08/30 15:45:01 by ltressen         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef CUB3D_H
 # define CUB3D_H
 
+# include "libft/libft.h"
 # include <stdio.h>
+# include "minilibx-linux/mlx.h"
+# include <math.h>
+# include <X11/X.h>
+# include <X11/keysym.h>
+
+# define HEIGHT 1080
+# define WIDTH 1920
+
+typedef struct s_cub
+{
+	void	*mlx_ptr;
+	void	*win_ptr;
+}	t_cub;
 
 #endif
--- main.c
+++ main.c
@@ -6,13 +6,41 @@
 /*   By: ltressen <ltressen@student.42perpignan.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/30 13:11:02 by ltressen          #+#    #+#             */
-/*   Updated: 2023/08/30 13:14:31 by ltressen         ###   ########.fr       */
+/*   Updated: 2023/08/30 15:52:47 by ltressen         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cub3d.h"
 
-int	main(void)
+int	key_events(int key, t_cub *cub)
 {
+	if (key == XK_Escape)
+	{
+		exit(0);
+	}
+	return (0);
+}
+
+int	close_cross(t_cub *cub)
+{
+	//free_all(cub);
+	exit(EXIT_SUCCESS);
+	return (0);
+}
+
+int	main(int ac, char **av)
+{
+	t_cub	cub;
+
+	if (ac == 1)
+	{
+		cub.mlx_ptr = mlx_init();
+		cub.win_ptr = mlx_new_window(cub.mlx_ptr, WIDTH, HEIGHT, "CubEZ v0.0");
+		mlx_key_hook(cub.win_ptr, key_events, &cub);
+		mlx_hook(cub.win_ptr, 17, 0L, close_cross, &cub);
+		mlx_loop(cub.mlx_ptr);
+		mlx_destroy_display(cub.mlx_ptr);
+		free(cub.mlx_ptr);
+	}
 	return (1);
 }
