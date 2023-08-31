6c6552de6fddea5b796259c05f65faa88fc43e63
Jordan Casadessus <jcasades@c1r1p5.42perpignan.fr>
2023-08-31 16:08:30 +0200

    .h

--- cub3d.h
+++ cub3d.h
@@ -6,13 +6,29 @@
 /*   By: ltressen <ltressen@student.42perpignan.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/30 13:11:49 by ltressen          #+#    #+#             */
-/*   Updated: 2023/08/30 13:12:35 by ltressen         ###   ########.fr       */
+/*   Updated: 2023/08/30 16:01:45 by jcasades         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef CUB3D_H
 # define CUB3D_H
 
+# include "libft/libft.h"
 # include <stdio.h>
+# include "minilibx-linux/mlx.h"
+# include <math.h>
+# include <X11/X.h>
+# include <X11/keysym.h>
+
+# define HEIGHT 1080
+# define WIDTH 1920
+
+typedef struct s_cub
+{
+	void	*mlx_ptr;
+	void	*win_ptr;
+}	t_cub;
+
+#endif
 
 #endif
