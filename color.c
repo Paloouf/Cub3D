6eccfb6577bc0d4b8c72ee7e1ac1233685575ac2
ltressen <paloouf@gmail.com>
2023-10-10 14:38:08 +0200

    on commence la norme

--- color.c
+++ color.c
@@ -6,13 +6,13 @@
 /*   By: ltressen <ltressen@student.42perpignan.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/14 11:19:13 by jcasades          #+#    #+#             */
-/*   Updated: 2023/10/09 15:40:07 by ltressen         ###   ########.fr       */
+/*   Updated: 2023/10/10 14:37:21 by ltressen         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cub3d.h"
 
-int	ft_color_f(t_cub *cub, int i , int x)
+int	ft_color_f(t_cub *cub, int i, int x)
 {
 	int	red;
 	int	green;
@@ -21,14 +21,14 @@ int	ft_color_f(t_cub *cub, int i , int x)
 	red = cub->fl.r_f;
 	blue = cub->fl.b_f;
 	green = cub->fl.g_f;
-	return(rgba_to_int(red, green, blue, 1));
+	return (rgba_to_int(red, green, blue, 1));
 }
 
 int	ft_color_c(t_cub *cub, int i, int x)
 {
-        int     red;
-        int     green;
-        int     blue;
+	int	red;
+	int	green;
+	int	blue;
 	
 	if (x > (WIDTH / 2))
 		x = WIDTH - x;
@@ -36,5 +36,5 @@ int	ft_color_c(t_cub *cub, int i, int x)
 	red = (cub->cl.r_c / (1 + (double)(i / (HEIGHT / 12))));
 	blue = (cub->cl.b_c / (1 + (double)(i / (HEIGHT / 12))));
 	green = (cub->cl.g_c / (1 + (double)(i / (HEIGHT / 12))));
-	return(rgba_to_int(red, green, blue, 1));
+	return (rgba_to_int(red, green, blue, 1));
 }
